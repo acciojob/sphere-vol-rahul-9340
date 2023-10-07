e3c51b43ad8b67b12aecfce22441b5c68e90feb8
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-07 12:47:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function volume_sphere() {
-    //Write your code here
-  
+
+function volume_sphere(event) {
+	event.preventDefault()
+	const input1 = document.getElementById("radius");
+	const input2 = document.getElementById("volume");
+	input2.value = "";
+	const pi = 3.14159 ;
+    let r = input1.value;
+	let sphere = 4/3*pi*r*r*r;
+     input2.value = sphere;
 } 
+
+
+// let submitbtn = document.getElementById("submit");
+
+// submitbtn.addEventListener("click",volume_sphere)
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
