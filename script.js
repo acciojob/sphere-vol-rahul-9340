963a27b342c7ab2fa0f2168f8f0a8bc02b2c8827
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-07 14:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function volume_sphere(event) {
 	const input1 = document.getElementById("radius");
 	const input2 = document.getElementById("volume");
 	input2.value = "";
-	const pi = 3.14159 ;
-    let r = parseFloat(input1.value);
-	let sphere = (4/3)*pi*r*r*r;
+	const pi = 3.141592653589793238462643383279502884197;
+    let r = input1.value;
+	let sphere = (4/3)*pi*(r*r*r);
      input2.value = sphere.toFixed(4);
 } 
 
